a483e799e23abbf4695ca202affb0dff77902a5e
Chanyoung Park <rhdwkrxla68@gmail.com>
2024-04-11 21:36:29 +0900

    Add: mui 패키지 확장 다운로드

--- src/app/components/ThemeProvider/ThemeProvider.tsx
+++ src/app/components/ThemeProvider/ThemeProvider.tsx
@@ -1,5 +1,10 @@
 'use client'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material';
 import { PropsWithChildren } from 'react';
 
